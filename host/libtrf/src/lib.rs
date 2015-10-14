ac72b62296e27a036f8980241f2a7080bdc509cc
André Wagner <andre.nho@gmail.com>
2015-10-14 05:02:19 -0300

    .

--- host/libtrf/src/lib.rs
+++ host/libtrf/src/lib.rs
@@ -2,8 +2,15 @@
 
 use std::vec::Vec;
 use std::collections::HashMap;
-use std::collections::hash_map::Entry;
-use std::fmt;
+
+//
+// HELPER FUNCTIONS
+//
+
+fn get32(a: &Vec<u8>, pos: u32) -> u32 {
+    let n = pos as usize;
+    (((a[n+3] as u32) << 24) | ((a[n+2] as u32) << 16) | ((a[n+1] as u32) << 8) | a[n] as u32)
+}
 
 // 
 // BINARY FILE
@@ -20,6 +27,8 @@ impl Binary {
     pub fn push8(&mut self, v: u8) { self.data.push(v); }
 
     pub fn push_vec(&mut self, v: Vec<u8>) { self.data.extend(v); }
+
+    pub fn push_slice(&mut self, v: &[u8]) { self.data.extend(v); }
 
     pub fn push_str(&mut self, s: &str) { self.push_vec(s.chars().map(|c| c as u8).collect()); }
 
@@ -49,18 +58,18 @@ impl Binary {
 // STRUCTURES
 //
 
-#[derive(Clone,Copy)]
+#[derive(Clone,Copy,PartialEq,Eq,Debug)]
 pub enum Scope {
     Global = 0, Local = 1, Extern = 2, PendingGlobal = 3, PendingLocal = 4,
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone,Copy,PartialEq,Eq,Debug)]
 pub enum ObjectType {
     Object = 0, Executable = 1, Kernel = 2, Library = 3
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Clone,Copy)]
+#[derive(Clone,Copy,PartialEq,Eq,Debug)]
 pub enum Section {
     text = 0, bss = 1, data = 2, rodata = 3, comment = 4, strtab = 5, symtab = 6, reloc = 7, debug = 8
 }
@@ -77,7 +86,8 @@ struct Sym {
 // TRF file
 //
 
-struct Symbol {
+#[derive(Clone,Copy,PartialEq,Eq,Debug)]
+pub struct Symbol {
     scope: Scope,
     section: Section
 }
@@ -92,8 +102,8 @@ pub struct TRFFile {
     pub rodata:  Binary,
     pub comment: Binary,
     pub debug:   Binary,
-    relocations: HashMap<String, Vec<u32>>,
-    symbols:     HashMap<String, Symbol>,
+    pub relocations: HashMap<String, Vec<u32>>,
+    pub symbols:     HashMap<String, Symbol>,
 }
 
 
@@ -115,6 +125,54 @@ impl TRFFile {
     }
 
 
+    pub fn from_binary(d: &Vec<u8>) -> Self {
+        let mut t = TRFFile::new();
+
+        if d.len() < 0x90 || d[0..4] != [0x7F, 0x54, 0x52, 0x46] { panic!("Not a TRF file"); }
+        if d[4] != 0x1 { panic!("Unsupported TRF version"); }
+
+        // header information
+        t.entry_point = ((d[0xB] as u32) << 24) | ((d[0xA] as u32) << 16) | ((d[0x9] as u32) << 8) | d[0x8] as u32;
+        t.object_type = match d[0x6] {
+            0x0 => ObjectType::Object,
+            0x1 => ObjectType::Executable,
+            0x2 => ObjectType::Kernel,
+            0x3 => ObjectType::Library,
+            _   => panic!("Invalid object type")
+        };
+
+        // sections
+        for i in 0x00..0x8 {
+            let n = 0x10+(i*8);
+            let pos = get32(&d, n) as usize;
+            let sz = get32(&d, n+4) as usize;
+            match i {
+                0x0 => t.text.push_slice(&d[pos..(pos+sz)]),
+                0x1 => t.bss.push_slice(&d[pos..(pos+sz)]),
+                0x2 => t.data.push_slice(&d[pos..(pos+sz)]),
+                0x3 => t.rodata.push_slice(&d[pos..(pos+sz)]),
+                0x4 => t.comment.push_slice(&d[pos..(pos+sz)]),
+                0x8 => t.debug.push_slice(&d[pos..(pos+sz)]),
+                _   => ()
+            }
+        }
+
+        // symbols and relocations
+        let strtab_pos = get32(&d, 0x38);
+        let strtab_sz  = get32(&d, 0x3C);
+        let symtab_pos = get32(&d, 0x40);
+        let symtab_sz  = get32(&d, 0x44);
+        let reloc_pos  = get32(&d, 0x48);
+        let reloc_sz   = get32(&d, 0x4C);
+
+        for i in 0..(symtab_sz / 12) {
+            
+        }
+
+        t
+    }
+
+
     pub fn add_text_reloc(&mut self, symbol: &str) {
         let mut v = self.relocations.entry(symbol.to_string()).or_insert(vec![]);
         v.push(self.text.len());
@@ -123,8 +181,7 @@ impl TRFFile {
 
 
     pub fn add_symbol(&mut self, section: Section, symbol: &str, scope: Scope) {
-        let mut v = self.symbols.entry(symbol.to_string())
-                        .or_insert(Symbol { scope: scope, section: section });
+        self.symbols.entry(symbol.to_string()).or_insert(Symbol { scope: scope, section: section });
     }
 
 
@@ -195,7 +252,7 @@ impl TRFFile {
             }
         }
 
-        for u in unresolved {
+        for _ in unresolved {
             unimplemented!();
         }
 
@@ -240,9 +297,9 @@ impl TRFFile {
             p((0x10+(i*8)), format!("Section #{}", i).as_ref(), &b[((0x10+(i*8)) as usize)..((0x18+(i*8)) as usize)]);
         }
         for i in 0x00..0x10 {
-            let n = (0x10+(i*8));
-            let pos = (((b[n+3] as u32) << 24) | ((b[n+2] as u32) << 16) | ((b[n+1] as u32) << 8) | b[n] as u32) as usize;
-            let sz = (((b[n+7] as u32) << 24) | ((b[n+6] as u32) << 16) | ((b[n+5] as u32) << 8) | b[n+4] as u32) as usize;
+            let n = 0x10+(i*8);
+            let pos = get32(&b, n) as usize;
+            let sz = get32(&b, n+4) as usize;
             if pos != 0 {
                 p(pos as u32, format!("Contents #{}", i).as_ref(), &b[pos..(pos+sz)]);
             }
@@ -278,7 +335,7 @@ mod tests {
 
     #[test]
     fn empty_file() {
-        let mut f = TRFFile::new();
+        let f = TRFFile::new();
 
         let bin : Vec<u8> = vec![
             0x7F, 0x54, 0x52, 0x46, 0x01, 0x01, 0x00, 0x00,       // header, version, type
@@ -413,6 +470,91 @@ mod tests {
             0x05, 0x00, 0x00, 0x00,     // offset for reference to "hello" in .text is 0x5
             0x00, 0x00, 0x00, 0x00      // index 0x0 in symbol table
         ];
+        assert_eq!(f.generate_binary(), bin);
+    }
+
+    #[test]
+    #[should_panic]
+    fn from_invalid_bin() {
+        TRFFile::from_binary(&vec![0x0]);
+    }
+
+    #[test]
+    fn from_bin() {
+        let bin : Vec<u8> = vec![
+            0x7F, 0x54, 0x52, 0x46,         // header 
+            0x01, 0x01, 0x01, 0x00,         // version, CPU type, object type
+            0x09, 0x00, 0x00, 0x00,         // entry point
+            0x00, 0x00, 0x00, 0x00,         // reserved 
+
+            0x90, 0x00, 0x00, 0x00,         // section .text
+            0x1F, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,         // section .bss
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,         // section .data
+            0x00, 0x00, 0x00, 0x00,
+            0xAF, 0x00, 0x00, 0x00,         // section .rodata
+            0x06, 0x00, 0x00, 0x00,
+            0xB5, 0x00, 0x00, 0x00,         // section .comment
+            0x16, 0x00, 0x00, 0x00,
+            0xCB, 0x00, 0x00, 0x00,         // section .strtab
+            0x05, 0x00, 0x00, 0x00,
+            0xD0, 0x00, 0x00, 0x00,         // section .symtab
+            0x0C, 0x00, 0x00, 0x00,
+            0xDC, 0x00, 0x00, 0x00,         // section .reloc
+            0x08, 0x00, 0x00, 0x00,         
+            0x00, 0x00, 0x00, 0x00,         // section .debug
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // other sections
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+
+            // section .text
+            0x2A, 0x10, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00,   // movd [MMU_MCPY_ORIG], hello
+            0x2C, 0x14, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x02, 0xF0,   // movd [MMU_MCPY_DEST], [VD_TEXT]
+            0x2A, 0x18, 0x00, 0x00, 0xF0, 0x06, 0x00,               // movw [MMU_MCPY_SZ], 6
+            0x22, 0x1A, 0x00, 0x00, 0xF0, 0x01,                     // movb [MMU_MCPY], 1
+
+            // section .rodata
+            0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x21,     // "hello!"
+
+            // section .comment
+            0x47, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x65,     // "Generated by tas 1.0.0"
+            0x64, 0x20, 0x62, 0x79, 0x20, 0x74, 0x61, 0x73,
+            0x20, 0x31, 0x2e, 0x30, 0x2e, 0x30,
+
+            // section .strtab
+            0x68, 0x65, 0x6C, 0x6C, 0x6F,           // "hello"
+
+            // section .symtab
+            0x01,                       // symbol local
+            0x03,                       // section .rodata
+            0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,     // index in strtab: 0x0
+            0x05, 0x00, 0x00, 0x00,     // string length 0x5
+
+            // section .reloc
+            0x05, 0x00, 0x00, 0x00,     // offset for reference to "hello" in .text is 0x5
+            0x00, 0x00, 0x00, 0x00      // index 0x0 in symbol table
+        ];
+
+        let f = TRFFile::from_binary(&bin);
+        assert_eq!(f.entry_point, 0x9);
+        assert_eq!(f.object_type, ObjectType::Executable);
+
+        assert_eq!(f.text.data, &[ 0x2A, 0x10, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00,
+                                   0x2C, 0x14, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x02, 0xF0,
+                                   0x2A, 0x18, 0x00, 0x00, 0xF0, 0x06, 0x00,
+                                   0x22, 0x1A, 0x00, 0x00, 0xF0, 0x01 ]);
+        assert_eq!(f.rodata.data, &[ 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x21 ]);
+
+        assert_eq!(f.symbols.get("hello"), Some(&Symbol { scope: Scope::Local, section: Section::rodata }));
+        assert_eq!(f.relocations.get("hello"), Some(&vec![ 0x5 ]));
+
         assert_eq!(f.generate_binary(), bin);
     }
 }
