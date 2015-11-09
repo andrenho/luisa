1fb9ab6a8074452ddc0731aa1c3e002b62877c1a
André Wagner <andre.nho@gmail.com>
2015-11-09 19:56:57 -0200

    .

--- host/trflib/tests.cc
+++ host/trflib/tests.cc
@@ -31,7 +31,7 @@ TEST_CASE("empty file", "[libtrf]") {
 }
 
 
-TEST_CASE("code", "[libtrf]") {
+TEST_CASE("sections (simple)", "[libtrf]") {
     
     TRFFile f;
 
@@ -39,10 +39,14 @@ TEST_CASE("code", "[libtrf]") {
     f.text.push_back('B');
     f.text.push_back('C');
 
+    f.data.push_back('X');
+
     const vector<uint8_t> bin = { 
         0x7F, 'T', 'R', 'F', 1, 1, 0, 0,    // header, version, type
         0, 0, 0, 0, 0, 0, 0, 0,             // entry point, reserved
-        0x74, 0, 0, 0, 0x3, 0, 0, 0,             // section .text
+        0x74, 0, 0, 0, 0x3, 0, 0, 0,        // section .text
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0x77, 0, 0, 0, 0x1, 0, 0, 0,        // section .data
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
@@ -56,9 +60,7 @@ TEST_CASE("code", "[libtrf]") {
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        'A', 'B', 'C',
+        'A', 'B', 'C', 'X'
     };
 
     REQUIRE(f.GenerateBinary() == bin);
