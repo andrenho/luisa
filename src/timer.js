54443fe405ecdcdc2f37f70e3364a46ac5d45f2d
André Wagner <andre.nho@gmail.com>
2016-01-04 07:16:03 -0200

    Timers done!

--- src/timer.js
+++ src/timer.js
@@ -4,6 +4,14 @@ export default class Timer extends Device {
 
   constructor(units) {
     super();
+
+    this._const = this.constantList();
+    this._dateDiff = new Date(2016, 1, 1).valueOf();
+    this._currentTime = Date.now() - this._dateDiff;
+    this._timers = new Uint32Array(10);
+    this._counters = new Uint32Array(10);
+    this._lastStep = Date.now();
+    this._interruptCalled = 0;
   }
 
   name() { return 'TinyTimer'; }
@@ -17,12 +25,53 @@ export default class Timer extends Device {
       TM_VERSION:    0x01,
       TM_INTERRUPT:  0x02,
       TM_NAME:       0x03,
+      TM_CLOCK:      0x10,
+      TM_TIMER0:     0x14,
+      TM_TIMER1:     0x18,
+      TM_TIMER2:     0x1C,
+      TM_TIMER3:     0x20,
+      TM_TIMER4:     0x24,
+      TM_TIMER5:     0x28,
+      TM_TIMER6:     0x2C,
+      TM_TIMER7:     0x30,
+      TM_TIMER8:     0x34,
+      TM_TIMER9:     0x38,
+      TM_COUNTER0:   0x3C,
+      TM_COUNTER1:   0x40,
+      TM_COUNTER2:   0x44,
+      TM_COUNTER3:   0x48,
+      TM_COUNTER4:   0x4C,
+      TM_COUNTER5:   0x50,
+      TM_COUNTER6:   0x54,
+      TM_COUNTER7:   0x58,
+      TM_COUNTER8:   0x5C,
+      TM_COUNTER9:   0x60,
+      TM_CUR_INT:    0x64,
     };
   }
 
   get(a) {
     if (a < 0x10) {
       return super.get(a);
+    } else if (a === this._const.TM_CUR_INT) {
+      return this._interruptCalled;
+    } else {
+      let v;
+      if (a >= this._const.TM_CLOCK && a < this._const.TM_CLOCK + 4) {
+        v = this._currentTime;
+      } else if (a >= this._const.TM_TIMER0 && a < this._const.TM_TIMER9 + 4) {
+        v = this._timers[Math.floor((a - this._const.TM_TIMER0) / 4)];
+      } else if (a >= this._const.TM_COUNTER0 && a < this._const.TM_COUNTER9 + 4) {
+        v = this._timers[Math.floor((a - this._const.TM_COUNTER0) / 4)];
+      }
+      if (v !== undefined) {
+        switch (a % 4) {
+          case 0: return v & 0xFF;
+          case 1: return (v >> 8) & 0xFF;
+          case 2: return (v >> 16) & 0xFF;
+          case 3: return (v >> 24) & 0xFF;
+        }
+      }
     }
     return 0;
   }
@@ -31,7 +80,55 @@ export default class Timer extends Device {
   set(a, v) {
     if (a < 0x10) {
       super.set(a, v);
+    } else {
+      let r;
+      if (a >= this._const.TM_TIMER0 && a < this._const.TM_TIMER9 + 0x20) {
+        r = Math.floor((a - this._const.TM_TIMER0) / 4);
+      }
+      if (r !== undefined) {
+        switch (a % 4) {
+          case 0: 
+            this._timers[r] &= ~0xFF; this._timers[r] |= v;
+            this._counters[r] = 0;
+            break;
+          case 1: 
+            this._timers[r] &= ~0xFF00; this._timers[r] |= (v << 8);
+            this._counters[r] = 0;
+            break;
+          case 2: 
+            this._timers[r] &= ~0xFF0000; this._timers[r] |= (v << 16);
+            this._counters[r] = 0;
+            break;
+          case 3: 
+            this._timers[r] &= ~0xFF000000; this._timers[r] |= (v << 24);
+            this._counters[r] = 0;
+            break;
+        }
+      }
     }
+  }
+
+
+  step() {
+    // We update the current time here. This way, the CPU can
+    // do an atomic read in the register, without worring about
+    // the time changing between successive reads of the 4 bytes.
+    this._currentTime = Date.now() - this._dateDiff;
+
+    // update counters
+    const now = Date.now();
+    for (let i = 0; i < 10; ++i) {
+      if (this._timers[i] !== 0) {
+        this._counters[i] += (now - this._lastStep);
+
+        if (this._counters[i] >= this._timers[i]) {
+          this._counters[i] = 0;
+          this._interruptCalled = i;
+          this.fireInterrupt();
+        }
+      }
+    }
+    this._lastStep = now;
   }
 
 }
--- test/timer.js
+++ test/timer.js
@@ -18,8 +18,9 @@ function makeTimer() {
 }
 
 
-function convertTime(ms, sec, min) {
-  return (ms | (sec << 10) | (min << 20));
+function sleep(ms) {
+  const d = (Date.now() + ms) >>> 0;
+  while(d > (Date.now() >>> 0)) { }   // busy wait
 }
 
 
@@ -29,6 +30,53 @@ test('Timer: sanity', t => {
   t.end();
 });
 
+
+test('Timer: clock', t => {
+  const [mb, cpu, tm] = makeTimer(); 
+  const d = new Date(2016, 1, 1).valueOf();
+
+  mb.step();
+  
+  const cp_now = (Date.now() - d) >>> 0;
+  const vm_now = mb.get32(tm.TM_CLOCK);
+
+  t.ok((vm_now >= cp_now - 10000) && (vm_now <= cp_now + 10000), 
+      `times match 0x${vm_now.toString(16)} (VM) vs 0x${cp_now.toString(16)}`);
+
+  t.end();
+});
+
+
+test('Timer: counter', t => {
+  const [mb, cpu, tm] = makeTimer(); 
+  
+  mb.set32(tm.TM_TIMER0, 1000);
+  sleep(100);
+  mb.step();
+  const v = mb.get32(tm.TM_COUNTER0);
+  t.notEqual(v, 0, 'counter != 0');
+  t.ok(v > 50, 'counter > 50');
+
+  t.end();
+});
+
+
+test('Timer: interrupt', t => {
+  const [mb, cpu, tm] = makeTimer(); 
+
+  mb.set(0x0, 0x86);  // nop
+  mb.set32(cpu.CPU_INTERRUPT_VECT + (4 * mb.get(tm.TM_INTERRUPT)), 0x1000);
+  cpu.T = true;
+
+  mb.set32(tm.TM_TIMER1, 50);  // setup timer
+  sleep(100);
+  mb.step();
+
+  t.equals(cpu.PC, 0x1000, 'interrupt was called');
+  t.equals(mb.get(tm.TM_CUR_INT), 0x1, 'the correct interrupt was called');
+
+  t.end();
+});
 
 //test('Timer: single timer', t => {
 
