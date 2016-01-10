093a2f18654b9f6a2e66d5f50e5911a908af97d2
André Wagner <andre.nho@gmail.com>
2016-01-10 00:41:20 -0200

    .

--- src/test/assembler.js
+++ src/test/assembler.js
@@ -1,9 +1,9 @@
 import test from 'tape';
 
-import assemblyToTif from '../utils/assemblyToTifr.js';
+import assemblyToTif from '../utils/assembler.js';
 
 
-test.only('TinyVM assemblyToTifr: valid inputs', t => {
+test.only('TinyVM assembler: valid inputs', t => {
 
   let file, result;
 
@@ -12,13 +12,12 @@ test.only('TinyVM assemblyToTifr: valid inputs', t => {
 .section text
         nop       ; some comment`;
   result = {
-    entry: 0,
-    code: [0x87],
+    text: [0x87],
   };
   t.deepEquals(assemblyToTif(file), result, 'simplest file');
 
   
-  // TODO - some useful code
+  // some useful code
   file = `
 .entry  0x1000
 .section text
@@ -26,18 +25,39 @@ test.only('TinyVM assemblyToTifr: valid inputs', t => {
         movb    A, [B]
         movb    A, [0x12345678]
         movd    [0xABCDEF01], [0x12345678]
-        or      K, 0x42
+        or      K, 0x4212
         not     F
         bz      0x42
         ret`;
+  result = {
+    entry: 0x1000,
+    text: [0x02, 0x00, 0x04,
+           0x05, 0x00, 0x01,
+           0x06, 0x00, 0x78, 0x56, 0x34, 0x12,
+           0x2C, 0x01, 0xEF, 0xCD, 0xAB, 0x78, 0x56, 0x34, 0x12,
+           0x2F, 0x12, 0x42,
+           0x41, 0x05,
+           0x5D, 0x42,
+           0x74],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'useful code');
 
-  // TODO - decimal, binary, hexa, negative numbers
+
+  // decimal, binary, hexa, negative numbers
   file = `
 .section text
         mov     A, 42
         mov     A, 0x42
         mov     A, 0b1010_1111
         mov     A, -42`;
+  result = {
+    text: [0x02, 0x2A,
+           0x02, 0x42,
+           0x02, 0xAF,
+           0x04, 0xD6, 0xFF, 0xFF, 0xFF],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'numeric representation');
+
 
   // bss section
   file = `
@@ -49,7 +69,7 @@ test.only('TinyVM assemblyToTifr: valid inputs', t => {
         .resd     4`;
   result = {
     entry: 0,
-    code: [0x87],
+    text: [0x87],
     bss: 26,
   };
   t.deepEquals(assemblyToTif(file), result, 'bss section');
@@ -66,7 +86,7 @@ test.only('TinyVM assemblyToTifr: valid inputs', t => {
         .dd     0xABCDEF01`;
   result = {
     entry: 0,
-    code: [0x87],
+    text: [0x87],
     data: [0x12, 0x34, 0x34, 0x12],
     rodata: [0x01, 0xEF, 0xCD, 0xAB],
   };
@@ -82,13 +102,13 @@ test.only('TinyVM assemblyToTifr: valid inputs', t => {
         .asciz  "Abc"`;
   result = {
     entry: 0,
-    code: [0x87],
+    text: [0x87],
     data: [65, 98, 99, 13, 65, 98, 99, 0],
   };
   t.deepEquals(assemblyToTif(file), result, 'ascii data');
 
 
-  // TODO - resolved labels in code
+  // resolved labels in code
   file = `
 .section text
 label:  nop
@@ -97,37 +117,112 @@ label:  nop
         nop
 fwd_label:
         nop`;
+  result = {
+    text: [0x87,
+           0x71, 'label', 0x00, 0x00, 0x00,
+           0x71, 'fwd_label', 0x00, 0x00, 0x00,
+           0x87,
+           0x87],
+    symbols: {
+      label: { section: 'text', addr: 0x00 },
+      fwd_label: { section: 'text', addr: 0x06 },
+    },
+  };
+  t.deepEquals(assemblyToTif(file), result, 'static labels');
+  
 
-  // TODO - local labels
+  // local labels
   file = `
 .section text
         nop
 .test1: jmp     .test1
 new:    nop
 .test1: jmp     .test1`;
+  result = {
+    text: [0x87,
+           0x71, '.test1', 0x00, 0x00, 0x00,
+           0x87,
+           0x71, 'new.test1', 0x00, 0x00, 0x00],
+    symbols: {
+      '.test1': { section: 'text', addr: 0x01 },
+      'new.test1': { section: 'text', addr: 0x07 },
+    },
+  };
+  t.deepEquals(assemblyToTif(file), result, 'local labels');
 
-  // TODO - resolved labels in data
+
+  // resolved labels in data
   file = `
 .section text
         movb    A, [ldat]
 .section data
 ldat:   db      0x1`;
+  result = {
+    text: [0x06, 0x00, 'ldata', 0x00, 0x00, 0x00],
+    data: [0x01],
+    symbols: {
+      'ldat': { section: 'data', addr: 0x00 },
+    },
+  };
+  t.deepEquals(assemblyToTif(file), result, 'data labels');
 
-  // TODO - exports
+
+  // exports
   file = `
 .section text
 @test:  nop`;
+  result = {
+    text: [0x87],
+    symbols: {
+      '@test': { section: 'text', addr: 0x00 },
+    },
+    exports: ['@test'],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'global labels');
 
-  // TODO - unresolved symbols
+
+  // unresolved symbols
   file = `
 .section text
-        jmp     test`;
+        jmp     @test`;
+  result = {
+    text: [0x71, '@test', 0x00, 0x00, 0x00],
+    unresolved: ['@test'],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'unresolved symbols');
   
 
-  // TODO - symbols
-  
+  // constants
+  file = `
+TEST = 0x1234
+.section text
+        jmp     TEST`;
+  result = {
+    text: [0x71, 0x34, 0x12, 0x00, 0x00],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'local constants');
 
-  // TODO - include files
+
+  // include files
+  file = `
+.import test/test1.ts
+.section text
+        jmp     TEST`;
+  result = {
+    text: [0x71, 0x34, 0x12, 0x00, 0x00],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'include files');
+
+
+  // include multiple files
+  file = `
+.import test/test2.ts
+.section text
+        jmp     TEST`;
+  result = {
+    text: [0x71, 0x34, 0x12, 0x00, 0x00],
+  };
+  t.deepEquals(assemblyToTif(file), result, 'include nested files');
 
 
   t.end();
--- src/utils/assembler.js
+++ src/utils/assembler.js
@@ -1,4 +1,11 @@
-export default function assemblyToIr(code) {
+// TIF : Tiny Internal Format (acutally a JSON object)
+// TRF : Tiny Relocatable Format (executables, object files and libraries)
+
+//
+// CONVERT ASSEMBLY TO TIF
+//
+
+export default function assemblyToTif(code) {
   return {};
 }
 
