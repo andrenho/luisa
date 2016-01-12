316395bf9bae5cee2b0cc6b7edc4ac1e23f2e987
André Wagner <andre.nho@gmail.com>
2016-01-12 18:18:27 -0200

    .

---
+++ src/test/linker.js
@@ -0,0 +1,12 @@
+import test from 'tape';
+
+import { resolveLifs, lifToLrf } from '../utils/linker';
+
+test('LuisaVM linker: resolve LIF', t => {
+
+  
+
+  t.end();
+});
+
+// vim: ts=2:sw=2:sts=2:expandtab
---
+++ src/utils/linker.js
@@ -0,0 +1,19 @@
+// LIF - Luisa Intermediate Format
+// LRF = Luisa Relocatable Format
+
+
+//
+// Unify several LIF files, resolving the pending symbols between them.
+//
+export function resolveLifs(lifList) {
+}
+
+
+// 
+// Convert a unified LIF to a LRF file
+//
+export function lifToLrf(lif) {
+}
+
+
+// vim: ts=2:sw=2:sts=2:expandtab
