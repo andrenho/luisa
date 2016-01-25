031aa4671a9cadc51957dc5212066ea9aeddb6f3
Andre Wagner <wagnerandre@johndeere.com>
2016-01-25 17:21:01 -0200

    .

--- computer.js
+++ computer.js
@@ -4,15 +4,82 @@ class Computer {
 
     constructor(mem_size_kb) {
         this.mem_size_kb = mem_size_kb;
+        this.reset();
     }
+
+    reset() {
+        this.mem = new Uint8Array(this.mem_size_kb * 1024);
+        this.out_of_bounds = false;
+    }
+
+    set(addr, value) {
+        if(addr > this.mem_size_kb * 1024) { 
+            this.out_of_bounds = true;
+        } else {
+            this.mem[addr] = value;
+        }
+    }
+
+    get(addr) {
+        if(addr > this.mem_size_kb * 1024) { 
+            this.out_of_bounds = true;
+            return 0;
+        } else {
+            return this.mem[addr];
+        }
+    }
+
+    set32(addr, value) {
+        if(addr + 3 > this.mem_size_kb * 1024) { 
+            this.out_of_bounds = true;
+        } else {
+            this.mem[addr] = value & 0xff;
+            this.mem[addr+1] = (value >> 8) & 0xff;
+            this.mem[addr+2] = (value >> 16) & 0xff;
+            this.mem[addr+3] = value >> 24;
+        }
+    }
+
+    get32(addr, value) {
+        if(addr + 3 > this.mem_size_kb * 1024) { 
+            this.out_of_bounds = true;
+            return 0;
+        } else {
+            return (this.mem[addr+3] << 24)   |
+                   (this.mem[addr+2] << 16) |
+                   (this.mem[addr+1] << 8)  |
+                   this.mem[addr];
+        }
+    }
+
+    // 
+    // tests
+    //
 
     run_tests(section) {
         var te = new TestEnvironment(section);
-        te.test('Simple test', 
-                [ [ _ => { return 1; }, '=', 1 ],
-                  [ _ => { return 'x'; }, '=', 'x' ] ])
-        te.test('Memory amount',
-                [ [ t => { return t.mem_size_kb; }, '=', 4, this ] ])
+        te.test('Memory amount (kB)',
+                [ [ t => { return t.mem_size_kb; }, '=', 4, this ] ]);
+        te.test('Getting/setting data (8-bit)',
+                [ [ t => { t.set(0xAB, 42); return t.get(0xAB); }, '=', 42, this ] ]);
+        te.test('Getting/setting data (32-bit)',
+                [ [ t => { 
+                        t.set(0x4, 0x01); 
+                        t.set(0x5, 0x23); 
+                        t.set(0x6, 0x45); 
+                        t.set(0x7, 0x67); 
+                        return t.get32(0x4);
+                }, '=', 0x67452301, this ],
+                  [ t => {
+                        t.set32(0x8, 0x89ABCDEF);
+                        return [ t.get(0x8), t.get(0xB) ];
+                }, '=', [0xEF, 0x89], this ] ]);
+        te.test('Out of bounds',
+                [ 
+                    [ t => { t.get(0xF0); return t.out_of_bounds; }, '=', false, this ],
+                    [ t => { t.get(0xF000000); return t.out_of_bounds; }, '=', true, this ],
+                ]);
+        this.reset();
     }
 
 }
--- tests.js
+++ tests.js
@@ -16,6 +16,17 @@ class TestEnvironment {
     }
 
     test(desc, tests) {
+
+        let fmt = function(s) {
+            if(typeof s === 'number') {
+                return '0x' + s.toString(16);
+            } else if(typeof s === 'object') {
+                return '[' + s.map(v => fmt(v)).join(', ') + ']';
+            } else {
+                return s;
+            }
+        }
+
         let s = ['<tr><td rowspan="' + tests.length + '">' + desc + '</td>'];
 
         let fst = true;
@@ -23,10 +34,42 @@ class TestEnvironment {
             if(!fst) { s.push('<tr>'); }
             
             let test_function = t[0], validation = t[1], expected = t[2], data = t[3];
-            s.push('<td style="text-align: center;">' + expected + '</td>');
-            let result = test_function(data);  // run test
-            s.push('<td style="text-align: center;">' + result + '</td>');
-            if(expected === result) {
+
+            // run test
+            let result;
+            try {
+                result = test_function(data);  // run test
+            } catch(e) {
+                result = e;
+            }
+
+            // verify assertion
+            let ok = true;
+            if(validation === '=') {
+                if(typeof expected === 'object') {
+                    if(typeof result != 'object') {
+                        ok = false;
+                    } else if(result.length != expected.length) {
+                        ok = false;
+                    } else {
+                        for(let i=0; i<result.length; ++i) {
+                            if(expected[i] != result[i]) {
+                                ok = false;
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    ok = (result === expected);
+                }
+            } else {
+                throw 'Invalid validation type';
+            }
+
+            //  add to table
+            s.push('<td style="text-align: center;">' + fmt(expected) + '</td>');
+            s.push('<td style="text-align: center;">' + fmt(result) + '</td>');
+            if(ok) {
                 s.push('<td style="text-align: center;"><img src="img/ok.png" alt="ok"></td>');
             } else {
                 s.push('<td style="text-align: center;"><img src="img/failed.png" alt="failed"></td>');
