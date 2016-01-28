9bdd0426d15e45cc49b60a4f52473805e51e2202
André Wagner <andre.nho@gmail.com>
2016-01-28 21:06:05 -0200

    .

---
+++ tinyvm/mmu.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class MMU {
+    
+    constructor() {
+    }
+
+    name() {
+        return 'MMU';
+    }
+
+    get(addr) {
+    }
+
+    set(addr, uint_8) {
+    }
+    
+}
+
+// vim: ts=4:sw=4:sts=4:expandtab
