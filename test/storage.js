273c2a93e752566b442fab31828a7cc621e6f213
André Wagner <andre.nho@gmail.com>
2016-02-21 19:42:34 -0300

    .

--- test/storage.js
+++ test/storage.js
@@ -1,5 +1,9 @@
 import test from 'tape';
 
+import Motherboard from '../src/motherboard';
+import RAM from '../src/ram';
+import MMU from '../src/mmu';
+import CPU from '../src/cpu';
 import LSBStorage from '../src/lsbstorage';
 import Storage from '../src/storage';
 
@@ -11,10 +15,44 @@ class FakeDisk extends LSBStorage {
 };
 
 
+function makeStorage(units) {
+  const m = new Motherboard();
+  m.addDevice(new MMU(new RAM(256)));
+  const c = new CPU(m);
+  m.addDevice(c);
+  const s = new Storage(units);
+  m.addDevice(s);
+  return [m, c, s];
+}
+
+
 test('Storage: sanity', t => {
   t.doesNotThrow(() => new Storage([new FakeDisk()]), null, "storage created");
+  t.doesNotThrow(() => makeStorage([new FakeDisk()]), null, "storage created and added to motherboard");
   t.end();
 });
 
+
+test('Storage: unit list', t => {
+  const [mb1, cpu1, stg1] = makeStorage([]);
+  t.equals(mb1.get(stg1.STG_UNIT_LIST), 0, 'no storage units');
+
+  const [mb2, cpu2, stg2] = makeStorage([new FakeDisk()]);
+  t.equals(mb2.get(stg2.STG_UNIT_LIST), 0b1, 'one storage unit');
+
+  const [mb3, cpu3, stg3] = makeStorage([new FakeDisk()]);
+  t.equals(mb3.get(stg3.STG_UNIT_LIST), 0b11, 'two storage units');
+
+  t.end();
+});
+
+
+test('Storage: read/write (poll)', t => {
+  const [mb, cpu, stg] = makeStorage([new FakeDisk()]);
+
+
+  
+  t.end();
+});
 
 // vim: ts=2:sw=2:sts=2:expandtab
